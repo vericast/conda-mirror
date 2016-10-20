9f3ebcd8b17fdf05d6e64be9632a18368ebc0de9
Eric Dill <eric.dill@maxpoint.com>
2016-10-20 13:58:37 -0400

    Include run_tests script

--- test_conda_mirror.py
+++ test_conda_mirror.py
@@ -12,20 +12,22 @@ def test_regenerate_local_repo():
     print("Regenerating local repo")
     subprocess.check_call('python regenerate-repodata.py'.split())
 
-
-def test_get_repodata():
+@pytest.mark.parametrize('platform',
+                         ('linux-64', 'linux-32',
+                          'osx-64', 'win-32', 'win-64'))
+def test_get_repodata(platform):
     channel = "test"
     MOCK_TEMPLATE = 'https://conda.anaconda.org/{channel}/{platform}/repodata.json'
     with requests_mock.mock() as m:
-        for platform in ('linux-64', 'linux-32', 'osx-64', 'win-32', 'win-64'):
-            repodata = os.path.join('local-repo', platform, 'repodata.json')
-            with open(repodata, 'r') as f:
-                mock_address = MOCK_TEMPLATE.format(
-                    channel=channel,
-                    platform=platform
-                )
-                print('mock_address=%s' % mock_address)
-                m.get(mock_address, text=f.read())
+        repodata = os.path.join('local-repo', platform, 'repodata.json')
+        with open(repodata, 'r') as f:
+            mock_address = MOCK_TEMPLATE.format(
+                channel=channel,
+                platform=platform
+            )
+            print('mock_address=%s' % mock_address)
+            m.get(mock_address, text=f.read())
 
-        ret = conda_mirror.get_repodata(channel, 'linux-64')
+        ret = conda_mirror.get_repodata(channel, platform)
+        assert ret
 
