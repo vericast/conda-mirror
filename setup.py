2b7a0eb80c492aafaccf18b527e3bb8fb84ab3a9
Eric Dill <eric.dill@maxpoint.com>
2016-10-24 10:56:31 -0400

    Update repo url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version=conda_mirror.__version__,
     author_email='eric.dill@maxpoint.com',
     description='mirror an upstream conda channel to a local directory',
-    url='http://bitbucket.maxpointinteractive.com/mf/conda-mirror',
+    url='https://github.com/ericdill/conda-mirror',
     platforms='Cross platform (Linux, Mac OSX, Windows)',
     license='BSD 3-Clause',
     install_requires=[
