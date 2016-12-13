dc5fc03bd75a5dd7d6aee32b5748da41a922e441
Eric Dill <eric.dill@maxpoint.com>
2016-12-13 11:27:19 -0500

    Remove packages that fail their assertion

--- conda_mirror/conda_mirror.py
+++ conda_mirror/conda_mirror.py
@@ -206,16 +206,24 @@ def _validate(filename, md5, sha256, size):
 
     def _get_output(cmd):
         return subprocess.check_output(cmd).decode().strip().split()[0]
+    def _assert_or_remove(left, right, assertion_test):
+        try:
+            assert left == right
+        except AssertionError:
+            logger.error("Package validation failed for %s", assertion_test)
+            logger.error("%s != %s", left, right)
+            logger.error(pformat(traceback.format_exc()))
+            logger.error("Removing package %s", filename)
+            _remove_package(filename)
+        else:
+            logger.debug('%s check passed', assertion_test)
 
     if size:
-        assert size == os.stat(filename).st_size
-        logger.debug('size check passed')
+        _assert_or_remove(size, os.stat(filename).st_size, 'size')
     if md5:
-        assert md5 == _get_output(['md5sum', filename])
-        logger.debug('md5 check passed')
+        _assert_or_remove(md5, _get_output(['md5sum', filename]), 'md5')
     if sha256:
-        assert sha256 == _get_output(['sha2565sum', filename])
-        logger.debug('sha256 check passed')
+        _assert_or_remove(sha256, _get_output(['sha2565sum', filename]), 'sha256')
 
 
 def _download(url, target_directory, package_metadata=None, validate=True,
