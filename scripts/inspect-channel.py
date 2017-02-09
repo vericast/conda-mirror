e502db98c82bd9e2f0772d85fc2f1e6a37d46abe
Eric Dill <eric.dill@maxpoint.com>
2017-02-09 08:07:24 -0500

    Cleaning up the validation script

--- scripts/inspect-channel.py
+++ scripts/inspect-channel.py
@@ -38,6 +38,12 @@ def cli():
         action='store',
         help="The number of parallel processes to spin up"
     )
+    ap.add_argument(
+        '--cron',
+        action="store_true",
+        help="Disable print calls that don't do so well in logs",
+        default=False
+    )
     args = ap.parse_args()
 
     with open(join(args.pkgs_dir, 'repodata.json'), 'r') as f:
@@ -48,30 +54,38 @@ def cli():
                 for idx, pkg in enumerate(conda_packages))
     start = time.time()
     need_attention = []
+    print_every = len(conda_packages) / 100
+    if print_every < 100:
+        print_every = 100
     with Pool(int(args.num_workers)) as p:
         for i, ret in enumerate(p.imap_unordered(validate, pkg_iter)):
             elapsed = int(time.time()) - int(start) or 1
             pkgs_per_sec = int(i / elapsed) or 1
             eta = int((len(conda_packages) - i) / pkgs_per_sec)
-            msg = ('\r{0}/{1}   {2}s elapsed   {3} processed sec   {4}s remaining'.format(
+            msg = ('{0}/{1}   {2}s elapsed   {3} processed/sec   {4}s remaining'.format(
                 i, len(conda_packages), elapsed, pkgs_per_sec, eta))
-            sys.stderr.write(msg)
+            if not args.cron:
+                sys.stderr.write('\r%s' % msg)
+            else:
+                if i % print_every  == 0:
+                    print('%s\n'% msg)
             if ret is not None:
                 need_attention.append(ret)
-    print('%s packages need attention' % len(need_attention))
+    print('\n%s packages need attention' % len(need_attention))
     for package_path, problem_type, info in need_attention:
         if problem_type == METADATA:
             print("Removing %s because it is not in the local "
-                  "repodata.json" % package_path)
+                  "repodata.json" % (package_path), file=sys.stderr)
             os.remove(package_path)
         elif problem_type == PACKAGE_VALIDATION:
             print("Removing %s because it failed package validation with this "
-                  "reason: %s" % (package_path, info))
+                  "reason: %s" % (package_path, info), file=sys.stderr)
             os.remove(package_path)
             del repodata['packages'][os.path.basename(package_path)]
             # Update the repodata immediately
             cm._write_repodata(os.path.dirname(package_path), repodata)
 
+    print("All packages that require attention")
     pprint(need_attention)
 
 
