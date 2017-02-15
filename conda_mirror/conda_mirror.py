de7684edbc917432235915b1e68551ffca2fa68d
Eric Dill <eric.dill@maxpoint.com>
2017-02-15 17:32:15 -0500

    Delete unused code

--- conda_mirror/conda_mirror.py
+++ conda_mirror/conda_mirror.py
@@ -386,25 +386,6 @@ def _validate_packages(package_repodata, package_directory):
                       size=package_metadata.get('size'))
 
 
-def _remove_local_blacklisted(blacklist, local_dir):
-    """Removes any local conda packages that are blacklisted.
-
-    Parameters
-    ----------
-    blacklist : list
-        Packages that should not be in `local_dir`
-    local_dir : str
-        Local directory to check for blacklisted conda packages
-    """
-    # get list of current packages in folder
-    local_packages = _list_conda_packages(local_dir)
-    # if any are not in the final mirror list, remove them
-    for package_name in local_packages:
-        if package_name in blacklist:
-            _remove_package(os.path.join(local_dir, package_name),
-                            reason="Package is blacklisted")
-
-
 def main(upstream_channel, target_directory, temp_directory, platform,
          blacklist=None, whitelist=None):
     """
--- test/test_conda_mirror.py
+++ test/test_conda_mirror.py
@@ -120,24 +120,3 @@ def test_handling_bad_package(tmpdir, repodata):
     conda_mirror._validate_packages(anaconda_repodata, bad_pkg_root)
     assert bad_pkg_name not in os.listdir(bad_pkg_root)
 
-
-def test_local_blacklisted_package(tmpdir):
-    local_repo_root = tmpdir.mkdir('repo').strpath
-    pkg_root = os.path.join(local_repo_root, 'linux-64')
-    os.makedirs(pkg_root)
-    blacklisted_pkg_name = 'remove-1-0.tar.bz2'
-    non_blacklisted_pkg_name = 'keep-1-0.tar.bz2'
-    with bz2.BZ2File(os.path.join(pkg_root, blacklisted_pkg_name), 'wb') as f:
-        f.write("This is a blacklisted package".encode())
-    with bz2.BZ2File(os.path.join(pkg_root, non_blacklisted_pkg_name), 'wb') as f:
-        f.write("This is not a blacklisted package".encode())
-    blacklist = [blacklisted_pkg_name]
-
-    # Test removal of local blacklisted packages
-    conda_mirror.logger.info("Testing %s", blacklisted_pkg_name)
-    assert blacklisted_pkg_name in os.listdir(pkg_root)
-    conda_mirror._remove_local_blacklisted(blacklist, pkg_root)
-    assert blacklisted_pkg_name not in os.listdir(pkg_root)
-    assert non_blacklisted_pkg_name in os.listdir(pkg_root)
-
-
