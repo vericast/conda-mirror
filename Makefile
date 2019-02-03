d0e3ed5c2c963e7c4068b025f977ac5414afbdb9
Peter Parente <ParenteP@valassis.com>
2019-02-02 19:48:51 -0500

    REL: Release automation

--- Makefile
+++ Makefile
@@ -9,8 +9,19 @@ help:
 activate:
 	@echo "conda activate $(ENV_NAME)"
 
+clean: ## Make a clean source tree
+	-find . -name '*.pyc' -exec rm -fv {} \;
+	rm -rf conda_mirror/__pycache__ test/__pycache__ __pycache__ dist *.egg-info
+
 env: ## Make a conda development environment
 	conda create -n $(ENV_NAME) --file requirements.txt --file requirements-test.txt
+
+release: sdist ## Make a pypi release
+	python setup.py sdist
+	twine upload dist/*.tar.gz
+
+sdist: clean ## Make a source distribution
+	python setup.py sdist
 
 test: ## Make a test run
 	python run_tests.py -vxrs test/
--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from setuptools import find_packages, setup
 import versioneer
 
+try:
+    with open('README.md') as f:
+        long_description = f.read()
+except:
+    long_description = ''
+    print('Failed to load README.md as long_description')
+
 setup(
+    name='conda_mirror',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    name='conda_mirror',
     author="Eric Dill",
     packages=find_packages(),
     author_email='eric.dill@maxpoint.com',
-    description='mirror an upstream conda channel to a local directory',
-    url='https://github.com/maxpoint/conda-mirror',
-    platforms='Cross platform (Linux, Mac OSX, Windows)',
+    description='Mirror an upstream conda channel to a local directory',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/valassis-digital-media/conda-mirror',
+    platforms=['Linux', 'Mac OSX', 'Windows'],
     license='BSD 3-Clause',
     install_requires=[
         'requests',
